8f6bb0f695682ebedf369b9c60b6507fd9323a32
Francois BERENGER <unixjunkie@sdf.org>
2018-07-18 15:48:25 +0900

    Test_camltc: test harder

--- src/test_camltc.ml
+++ src/test_camltc.ml
@@ -2,42 +2,66 @@ open Printf
 
 let n = 1_000_000
 
-(* RWDBZ *)
-module Rwdbz = Dokeysto_lz4.Db_lz4.RWZ
+module Rwdb = Dokeysto_camltc.Db_camltc.RW
 
-let test_rwdbz () =
+let test_rwdb () =
   let start = Unix.gettimeofday () in
-  let rwz = Rwdbz.create "rwdb_lz4" in
+  let rw = Rwdb.create "rwdb_camltc" in
   for i = 1 to n do
     let s = string_of_int i in
-    Rwdbz.add rwz s s
+    Rwdb.add rw s s
   done;
   let stop = Unix.gettimeofday () in
-  printf "RWZ records creation rate: %.2f/s\n%!" (float n /. (stop -. start));
-  Rwdbz.sync rwz;
+  printf "RW records creation rate: %.2f/s\n%!" (float n /. (stop -. start));
+  Rwdb.sync rw;
   let start' = Unix.gettimeofday () in
   for i = 1 to n do
     let s = string_of_int i in
-    assert(s = Rwdbz.find rwz s);
+    assert(s = Rwdb.find rw s);
   done;
   let stop' = Unix.gettimeofday () in
-  printf "RWZ records find rate: %.2f/s\n%!" (float n /. (stop' -. start'));
-  let _ = Sys.command "ls -lh rwdb_lz4 rwdb_lz4.idx" in
-  Rwdbz.close rwz
-
-(* RODBZ *)
-module Rodbz = Dokeysto_lz4.Db_lz4.ROZ
-                 
-let test_rodbz () =
-  let roz = Rodbz.open_existing "rwdb_lz4" in
-  let start''' = Unix.gettimeofday () in
+  printf "RW records find rate: %.2f/s\n%!" (float n /. (stop' -. start'));
+  (* test iter *)
+  let count = ref 0 in
+  Rwdb.iter (fun k v ->
+      assert(k = v);
+      incr count
+    ) rw;
+  assert(!count = n);
+  (* test fold *)
+  let ref_acc = ref [] in
   for i = 1 to n do
     let s = string_of_int i in
-    assert(s = Rodbz.find roz s);
+    ref_acc := (s, s) :: !ref_acc
+  done;
+  let acc = Rwdb.fold (fun k v acc -> (k, v) :: acc) rw [] in
+  assert(List.length acc = n);
+  assert(List.sort compare !ref_acc = List.sort compare acc);
+  let _ = Sys.command "ls -lh rwdb_camltc" in
+  Rwdb.close rw
+
+module Rodb = Dokeysto_camltc.Db_camltc.RO
+
+let test_rodb () =
+  let ro = Rodb.open_existing "rwdb_camltc" in
+  let start''' = Unix.gettimeofday () in
+  let count = ref 0 in
+  for i = 1 to n do
+    let s = string_of_int i in
+    incr count;
+    assert(s = Rodb.find ro s);
   done;
   let stop''' = Unix.gettimeofday () in
-  printf "ROZ records find rate: %.2f/s\n%!" (float n /. (stop''' -. start'''))
+  assert(!count = n);
+  printf "RO records find rate: %.2f/s\n%!" (float n /. (stop''' -. start'''));
+  Rodb.close ro
 
 let () =
-  test_rwdbz ();
-  test_rodbz ()
+  test_rwdb ();
+  test_rodb ();
+  let rw = Rwdb.open_existing "rwdb_camltc" in
+  Rwdb.destroy rw;
+  assert(not (Sys.file_exists "rwdb_camltc"));
+  let dummy_rw = Rwdb.dummy () in
+  let _dummy_ro = Rodb.dummy () in
+  Rwdb.destroy dummy_rw
