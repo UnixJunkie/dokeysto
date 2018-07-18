e34755c6b1967af377751077e0d88b9ef7ee320c
Francois BERENGER <unixjunkie@sdf.org>
2018-07-18 15:07:24 +0900

    implemented Db_camltc.RO

--- src/db_camltc.ml
+++ src/db_camltc.ml
@@ -78,35 +78,42 @@ module RO = struct
   type t = db
 
   let open_existing fn =
-    failwith "not implemented yet"
-    (* RO.open_existing fn *)
+    let mode = Bdb.(oreader lor onolck) in
+    let bdb = Bdb.create ~mode fn [] in
+    { fn; bdb }
 
   let dummy () =
-    failwith "not implemented yet"
-    (* RO.dummy () *)
+    (* FBR: not sure this will work *)
+    let dummy_fn = Filename.temp_file "" "" in
+    open_existing dummy_fn
 
   let close db =
-    failwith "not implemented yet"
-    (* RO.close db *)
+    Bdb.close db.bdb
 
   let mem db k =
-    failwith "not implemented yet"
-    (* RO.mem db k *)
+    Bdb.exists db.bdb k
 
   let find db k =
-    failwith "not implemented yet"
-    (* RO.find db k *)
+    Bdb.get db.bdb k
 
   let iter f db =
-    failwith "not implemented yet"
-    (* RO.iter (fun k z ->
-     *     f k z
-     *   ) db *)
+    if Bdb.get_key_count db.bdb > Int64.one then
+      try
+        let cur = Bdb.get_cursor db.bdb in
+        Bdb.first db.bdb cur;
+        while true do
+          let k = Bdb.key db.bdb cur in
+          let v = Bdb.value db.bdb cur in
+          f k v;
+          Bdb.next db.bdb cur
+        done
+      with Not_found -> () (* end of db *)
 
-  let fold f db init =
-    failwith "not implemented yet"
-    (* RO.fold (fun k z acc ->
-     *     f k z acc
-     *   ) db init *)
+  let fold (f: string -> string -> 'a -> 'a) db init =
+    let acc = ref init in
+    iter (fun k v ->
+        acc := f k v !acc
+      ) db;
+    !acc
 
 end
