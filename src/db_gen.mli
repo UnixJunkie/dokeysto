a384f720b9ec468cf6f921b7817b8acb82bdc3b5
Francois Berenger <unixjunkie@sdf.org>
2020-01-10 16:26:34 +0900

    added src/db_gen.mli

---
+++ src/db_gen.mli
@@ -0,0 +1,96 @@
+ 
+type filename = string
+
+(** {4 Read-only persistent generic key to generic value hash table} *)
+
+module RO: sig
+
+  type t
+
+  (** [open_existing fn] open in read-only mode the persistent
+      hashtbl whose data are stored in file [fn] and whose
+      index is stored in [fn ^ ".idx"]. *)
+  val open_existing: filename -> t
+
+  (** [dummy ()] create a value of type [t].
+      Do not do anything with this value. *)
+  val dummy: unit -> t
+
+  (** [close db] close the previously opened [db]. *)
+  val close: t -> unit
+
+  (** [mem db k] check if [k] is bound in [db]. *)
+  val mem: t -> 'a -> bool
+
+  (** [find db k] get the current binding of [k] in [db]
+      or raise [Not_found]. *)
+  val find: t -> 'a -> 'b
+
+  (** [iter f db] apply [f] to all key-value pairs in [db].
+      Cf. Hashtbl.iter for details. *)
+  val iter: ('a -> 'b -> unit) -> t -> unit
+
+  (** [fold f db init] fold [f] over all key-value pairs in [db].
+      Cf. Hashtbl.fold for details. *)
+  val fold: ('a -> 'b -> 'c -> 'c) -> t -> 'c -> 'c
+
+end
+
+(** {4 Read-write persistent generic key to generic value hash table} *)
+
+module RW: sig
+
+  type t
+
+  (** [create fn] create in read-write mode the persistent
+      hashtbl whose data are stored in file [fn] and whose
+      index is stored in [fn ^ ".idx"]. *)
+  val create: filename -> t
+
+  (** [open_existing fn] open in read-write mode the persistent
+      hashtbl whose data are stored in file [fn] and whose
+      index is stored in [fn ^ ".idx"]. *)
+  val open_existing: filename -> t
+
+  (** [dummy ()] create a value of type [t].
+      Do not do anything with this value. *)
+  val dummy: unit -> t
+
+  (** [close db] close the previously opened [db]. *)
+  val close: t -> unit
+
+  (** [sync db] sync to disk the data and metadata (index) of [db]. *)
+  val sync: t -> unit
+
+  (** [destroy db] rm data and metadata files of [db] and clear [db]'s
+      index hashtbl. *)
+  val destroy: t -> unit
+
+  (** [mem db k] check if [k] is bound in [db]. *)
+  val mem: t -> 'a -> bool
+
+  (** [add db k v] add the key-value binding [(k,v)] to [db]. *)
+  val add: t -> 'a -> 'b -> unit
+
+  (** [replace db k v] replace the current binding for [k] in [db]
+      by a binding from [k] to [v].
+      Cf. Hashtbl.replace for details. *)
+  val replace: t -> 'a -> 'b -> unit
+
+  (** [remove tbl k] remove the current binding for [k] in [db].
+      Cf. Hashtbl.replace for details. *)
+  val remove: t -> 'a -> unit
+
+  (** [find db k] get the current binding of [k] in [db]
+      or raise [Not_found]. *)
+  val find: t -> 'a -> 'b
+
+  (** [iter f db] apply [f] to all key-value pairs in [db].
+      Cf. Hashtbl.iter for details. *)
+  val iter: ('a -> 'b -> unit) -> t -> unit
+
+  (** [fold f db init] fold [f] over all key-value pairs in [db].
+      Cf. Hashtbl.fold for details. *)
+  val fold: ('a -> 'b -> 'c -> 'c) -> t -> 'c -> 'c
+
+end
