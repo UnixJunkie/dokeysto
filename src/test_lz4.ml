140a529d74a3e505fb34cf1b972376e2054b8953
Francois Berenger <unixjunkie@sdf.org>
2020-01-20 15:26:43 +0900

    Test_lz4: added test_rwdbz_gen and test_rodbz_gen

--- src/test_lz4.ml
+++ src/test_lz4.ml
@@ -3,9 +3,8 @@ open Printf
 let n = 1_000_000
 
 (* RWDBZ *)
-module Rwdbz = Dokeysto_lz4.Db_lz4.RWZ
-
 let test_rwdbz () =
+  let module Rwdbz = Dokeysto_lz4.Db_lz4.RWZ in
   let start = Unix.gettimeofday () in
   let rwz = Rwdbz.create "rwdb_lz4" in
   for i = 1 to n do
@@ -25,10 +24,28 @@ let test_rwdbz () =
   let _ = Sys.command "ls -lh rwdb_lz4 rwdb_lz4.idx" in
   Rwdbz.close rwz
 
+let test_rwdbz_gen () =
+  let module Rwdbz_gen = Dokeysto_lz4.Db_lz4_gen.RWZ in
+  let start = Unix.gettimeofday () in
+  let rwz = Rwdbz_gen.create "rwdb_lz4_gen" in
+  for i = 1 to n do
+    Rwdbz_gen.add rwz i i
+  done;
+  let stop = Unix.gettimeofday () in
+  printf "RWZ_gen records creation rate: %.2f/s\n%!" (float n /. (stop -. start));
+  Rwdbz_gen.sync rwz;
+  let start' = Unix.gettimeofday () in
+  for i = 1 to n do
+    assert(i = Rwdbz_gen.find rwz i);
+  done;
+  let stop' = Unix.gettimeofday () in
+  printf "RWZ_gen records find rate: %.2f/s\n%!" (float n /. (stop' -. start'));
+  let _ = Sys.command "ls -lh rwdb_lz4_gen rwdb_lz4_gen.idx" in
+  Rwdbz_gen.close rwz
+
 (* RODBZ *)
-module Rodbz = Dokeysto_lz4.Db_lz4.ROZ
-                 
 let test_rodbz () =
+  let module Rodbz = Dokeysto_lz4.Db_lz4.ROZ in
   let roz = Rodbz.open_existing "rwdb_lz4" in
   let start''' = Unix.gettimeofday () in
   for i = 1 to n do
@@ -38,6 +55,18 @@ let test_rodbz () =
   let stop''' = Unix.gettimeofday () in
   printf "ROZ records find rate: %.2f/s\n%!" (float n /. (stop''' -. start'''))
 
+let test_rodbz_gen () =
+  let module Rodbz_gen = Dokeysto_lz4.Db_lz4_gen.ROZ in
+  let roz = Rodbz_gen.open_existing "rwdb_lz4_gen" in
+  let start''' = Unix.gettimeofday () in
+  for i = 1 to n do
+    assert(i = Rodbz_gen.find roz i);
+  done;
+  let stop''' = Unix.gettimeofday () in
+  printf "ROZ_gen records find rate: %.2f/s\n%!" (float n /. (stop''' -. start'''))
+
 let () =
   test_rwdbz ();
-  test_rodbz ()
+  test_rwdbz_gen ();
+  test_rodbz ();
+  test_rodbz_gen ()
