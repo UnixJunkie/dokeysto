9403d7a9c0d5add604730b142b58caeccf104ab8
Francois Berenger <unixjunkie@sdf.org>
2020-01-20 15:15:08 +0900

    Test_camltc: added test_rwdb_gen and test_rodb_gen

--- src/test_camltc.ml
+++ src/test_camltc.ml
@@ -40,6 +40,41 @@ let test_rwdb () =
   let _ = Sys.command "ls -lh rwdb_camltc" in
   Rwdb.close rw
 
+module Rwdb_gen = Dokeysto_camltc.Db_camltc_gen.RW (Dokeysto.Gen_gen)
+
+let test_rwdb_gen () =
+  let start = Unix.gettimeofday () in
+  let rw = Rwdb_gen.create "rwdb_camltc_gen" in
+  for i = 1 to n do
+    Rwdb_gen.add rw i i
+  done;
+  let stop = Unix.gettimeofday () in
+  printf "RW_gen records creation rate: %.2f/s\n%!" (float n /. (stop -. start));
+  Rwdb_gen.sync rw;
+  let start' = Unix.gettimeofday () in
+  for i = 1 to n do
+    assert(i = Rwdb_gen.find rw i);
+  done;
+  let stop' = Unix.gettimeofday () in
+  printf "RW_gen records find rate: %.2f/s\n%!" (float n /. (stop' -. start'));
+  (* test iter *)
+  let count = ref 0 in
+  Rwdb_gen.iter (fun k v ->
+      assert(k = v);
+      incr count
+    ) rw;
+  assert(!count = n);
+  (* test fold *)
+  let ref_acc = ref [] in
+  for i = 1 to n do
+    ref_acc := (i, i) :: !ref_acc
+  done;
+  let acc = Rwdb_gen.fold (fun k v acc -> (k, v) :: acc) rw [] in
+  assert(List.length acc = n);
+  assert(List.sort compare !ref_acc = List.sort compare acc);
+  let _ = Sys.command "ls -lh rwdb_camltc_gen" in
+  Rwdb_gen.close rw
+
 module Rodb = Dokeysto_camltc.Db_camltc.RO
 
 let test_rodb () =
@@ -56,9 +91,26 @@ let test_rodb () =
   printf "RO records find rate: %.2f/s\n%!" (float n /. (stop''' -. start'''));
   Rodb.close ro
 
+module Rodb_gen = Dokeysto_camltc.Db_camltc_gen.RO (Dokeysto.Gen_gen)
+
+let test_rodb_gen () =
+  let ro = Rodb_gen.open_existing "rwdb_camltc_gen" in
+  let start''' = Unix.gettimeofday () in
+  let count = ref 0 in
+  for i = 1 to n do
+    incr count;
+    assert(i = Rodb_gen.find ro i);
+  done;
+  let stop''' = Unix.gettimeofday () in
+  assert(!count = n);
+  printf "RO_gen records find rate: %.2f/s\n%!" (float n /. (stop''' -. start'''));
+  Rodb_gen.close ro
+
 let () =
   test_rwdb ();
+  test_rwdb_gen ();
   test_rodb ();
+  test_rodb_gen ();
   let rw = Rwdb.open_existing "rwdb_camltc" in
   Rwdb.destroy rw;
   assert(not (Sys.file_exists "rwdb_camltc"));
