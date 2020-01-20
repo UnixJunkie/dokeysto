a7b5030fd0e23437c5a1400c936590261dce05be
Francois Berenger <unixjunkie@sdf.org>
2020-01-20 12:45:56 +0900

    Db_camltc: moved the Internal module into its own Internal_camltc module

--- src/db_camltc.ml
+++ src/db_camltc.ml
@@ -3,103 +3,71 @@ module Bdb = Camltc.Bdb
 
 type filename = string
 
-type db = { fn: filename;
-            bdb: Bdb.bdb }
-
-module Internal = struct
-
-  let close db =
-    Bdb.close db.bdb
-
-  let mem db k =
-    Bdb.exists db.bdb k
-
-  let find db k =
-    Bdb.get db.bdb k
-
-  let iter f db =
-    if Bdb.get_key_count db.bdb > Int64.one then
-      try
-        let cur = Bdb.get_cursor db.bdb in
-        Bdb.first db.bdb cur;
-        while true do
-          let k = Bdb.key db.bdb cur in
-          let v = Bdb.value db.bdb cur in
-          f k v;
-          Bdb.next db.bdb cur
-        done
-      with Not_found -> () (* end of db *)
-
-  let fold (f: string -> string -> 'a -> 'a) db init =
-    let acc = ref init in
-    iter (fun k v ->
-        acc := f k v !acc
-      ) db;
-    !acc
-
-end
-
 module RW = struct
 
-  type t = db
+  type t = Internal_camltc.db
 
   let create fn =
     let mode = Bdb.(oreader lor owriter lor ocreat lor otrunc lor onolck) in
     let bdb = Bdb.create ~mode fn [] in
-    { fn; bdb }
+    Internal_camltc.{ fn; bdb }
 
   let open_existing fn =
     let mode = Bdb.(oreader lor owriter lor onolck) in
     let bdb = Bdb.create ~mode fn [] in
-    { fn; bdb }
+    Internal_camltc.{ fn; bdb }
 
   let dummy () =
     let dummy_fn = Filename.temp_file "" "" in
     create dummy_fn
 
   let sync db =
-    Bdb.sync db.bdb
+    Bdb.sync Internal_camltc.(db.bdb)
 
   let close db =
-    Internal.close db
+    Internal_camltc.close db
 
   let destroy db =
-    Bdb.close db.bdb;
-    Bdb.delete db.bdb;
-    Sys.remove db.fn
+    Internal_camltc.(
+      Bdb.close db.bdb;
+      Bdb.delete db.bdb;
+      Sys.remove db.fn
+    )
 
   let mem db k =
-    Internal.mem db k
+    Internal_camltc.mem db k
 
   let find db k =
-    Internal.find db k
+    Internal_camltc.find db k
 
   let add db k str =
-    Bdb.put db.bdb k str
+    Bdb.put Internal_camltc.(db.bdb) k str
 
   let replace db k str =
-    Bdb.out db.bdb k;
-    Bdb.put db.bdb k str
+    Internal_camltc.(
+      Bdb.out db.bdb k;
+      Bdb.put db.bdb k str
+    )
 
   let remove db k =
-    Bdb.out db.bdb k
+    Bdb.out Internal_camltc.(db.bdb) k
 
   let iter f db =
-    Internal.iter f db
+    Internal_camltc.iter f db
 
   let fold f db init =
-    Internal.fold f db init
+    Internal_camltc.fold f db init
 
 end
 
 module RO = struct
 
-  type t = db
+  type t = Internal_camltc.db
 
   let open_existing fn =
     let mode = Bdb.(oreader lor onolck) in
     let bdb = Bdb.create ~mode fn [] in
-    { fn; bdb }
+    Internal_camltc.{ fn; bdb }
 
   let dummy () =
     let db = RW.dummy () in
@@ -107,18 +75,18 @@ module RO = struct
     open_existing db.fn
 
   let close db =
-    Internal.close db
+    Internal_camltc.close db
 
   let mem db k =
-    Internal.mem db k
+    Internal_camltc.mem db k
 
   let find db k =
-    Internal.find db k
+    Internal_camltc.find db k
 
   let iter f db =
-    Internal.iter f db
+    Internal_camltc.iter f db
 
   let fold f db init =
-    Internal.fold f db init
+    Internal_camltc.fold f db init
 
 end
